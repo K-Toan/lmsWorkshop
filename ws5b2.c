442be8f12aebb9e692e070c73a2a7bf8ba374774
list <988letoangt@gmail.com>
2022-06-30 13:56:49 +0700

    Small changes

--- ws5b2.c
+++ ws5b2.c
@@ -2,6 +2,13 @@
 
 void stringReverse(char s[], int length)
 {
+    int index = 0;
+    /*
+    printf("The original string: ");
+    while(index < length)
+        printf("%c", s[index++]);
+    printf("\nThe reverse string: ");
+    */
     while(length >= 0)
         printf("%c", s[--length]);
 }
@@ -10,6 +17,7 @@ int main()
 {
     char s[200];
     int length = 0;
+    //scanf("%[^\n]s", s);
     while(1)
     {
         char c = getchar();
