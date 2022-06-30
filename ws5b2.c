c9066e8dda2c8b12fd9951c338b9bfe092aabe93
list <988letoangt@gmail.com>
2022-06-30 14:05:31 +0700

    Fix input

--- ws5b2.c
+++ ws5b2.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
+#include<string.h>
 
 void stringReverse(char s[], int length)
 {
     int index = 0;
-    /*
     printf("The original string: ");
     while(index < length)
         printf("%c", s[index++]);
     printf("\nThe reverse string: ");
-    */
     while(length >= 0)
         printf("%c", s[--length]);
 }
@@ -16,14 +15,6 @@ void stringReverse(char s[], int length)
 int main()
 {
     char s[200];
-    int length = 0;
-    //scanf("%[^\n]s", s);
-    while(1)
-    {
-        char c = getchar();
-        if((int)c == 10)
-            break;
-        s[length++] = c;
-    }
-    stringReverse(s, length);
+    scanf("%*c%[^\n]", s);
+    stringReverse(s, strlen(s));
 }
